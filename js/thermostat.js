18d35b7ef6c18f204b53f923e1f9d451ff7f3c44
Oliver <o.lucas@live.com>
2015-01-21 09:40:12 +0000

    minimum and maximum temperatures fully functional

--- js/thermostat.js
+++ js/thermostat.js
@@ -1,15 +1,26 @@
 var Thermostat = function() {
   this.temperature = 20;
-  this.maximumTemp = 32;
+  this.maximumTemp = 25;
   this.minimumTemp = 10;
+  this.powerSave = true;
 
 
 };
 
 Thermostat.prototype.increaseTemperature = function(changeTempBy) {
-  return this.temperature += changeTempBy;
+  if (this.temperature + changeTempBy > this.maximumTemp){
+    this.temperature = this.maximumTemp
+  }  
+  else {
+   this.temperature += changeTempBy;
+  }
 };
 
 Thermostat.prototype.decreaseTemperature = function(changeTempBy) {
-  return this.temperature -= changeTempBy;
-}
+  if (this.temperature + changeTempBy < this.minimumTemp){
+    this.temperature = this.minimumTemp
+  }
+  else {
+   this.temperature -= changeTempBy;
+  }
+};
