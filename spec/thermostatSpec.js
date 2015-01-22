b43d112f61e3417648f361b47473e54a68a54a9c
Oliver <o.lucas@live.com>
2015-01-22 12:15:17 +0000

    correct indentation implemented

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -13,49 +13,51 @@ describe("by default", function(){
   });
 });
 
-  describe("changing temperature", function(){
-    it("can increase", function(){
-      thermostat.increaseTemperature(1);
-      expect(thermostat.temperature).toEqual(21);
-    });
-    it("can decrease", function(){
-      thermostat.decreaseTemperature(1);
-      expect(thermostat.temperature).toEqual(19);
-    });
+describe("changing temperature", function(){
+  it("can increase", function(){
+    thermostat.increaseTemperature(1);
+    expect(thermostat.temperature).toEqual(21);
+  });
+  it("can decrease", function(){
+    thermostat.decreaseTemperature(1);
+    expect(thermostat.temperature).toEqual(19);
+  });
+});
+
+describe("minimum and maximum temp", function(){
+  it("should have a maximum of 32 degrees", function(){
+    expect(thermostat.maximumTemp).toEqual(32)
   });
 
-  describe("minimum and maximum temp", function(){
-    it("should have a maximum of 32 degrees", function(){
-      expect(thermostat.maximumTemp).toEqual(32)
-    });
-
-    it("should have a minimum of 10 degrees", function(){
-      expect(thermostat.minimumTemp).toEqual(10)
-    });
-
+  it("should have a minimum of 10 degrees", function(){
+    expect(thermostat.minimumTemp).toEqual(10)
   });
 
-  describe("reset button", function(){
+});
+
+describe("reset button", function(){
     
-    it("should reset temperature to 20 degrees", function(){
-      thermostat.resetButton;
-      expect(thermostat.temperature).toEqual(20)
-    });
-
+  it("should reset temperature to 20 degrees", function(){
+    thermostat.resetButton;
+    expect(thermostat.temperature).toEqual(20)
   });
 
-  describe("power save mode", function(){
+});
 
-    it("should set a limit of 25 degrees when on", function(){
-      thermostat.powerSave
-      expect(thermostat.maxTempPowerSave).toEqual(25)
-    });
+describe("power save mode", function(){
 
-    it("should have a limit of 32 degrees when off", function(){
-      powerSave = false
-      expect(thermostat.maximumTemp).toEqual(32)
-    });
+  it("should set a limit of 25 degrees when on", function(){
+    thermostat.powerSave
+    expect(thermostat.maxTempPowerSave).toEqual(25)
+  });
+
+  it("should have a limit of 32 degrees when off", function(){
+    powerSave = false
+    expect(thermostat.maximumTemp).toEqual(32)
+  });
      
-  });
+});
+
+  describe("")
 
 });
